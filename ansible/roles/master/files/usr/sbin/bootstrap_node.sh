851489b058eca10d17645e27924b9f78ef4e0e7d
Dan Dietz <dan@dandietz.com>
2019-11-04 11:46:35 +0000

    git settings

--- ansible/roles/master/files/usr/sbin/bootstrap_node.sh
+++ ansible/roles/master/files/usr/sbin/bootstrap_node.sh
@@ -10,6 +10,7 @@ mv ~ec2-user/id_rsa* /root/.ssh/
 mv ~ec2-user/.vaultpass /root/
 
 cd /root/
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 git clone git@github.com:ddietz89/cluster.git 
 ansible-playbook common.yml node.yml -i \"$1,\" --vault-password-file=/root/.vaultpass
 
