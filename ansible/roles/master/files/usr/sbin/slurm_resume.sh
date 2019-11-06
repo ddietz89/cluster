46f09d8b5c499f7fb95476ee8e3fa119e61f7821
Dan Dietz <dan@dandietz.com>
2019-11-06 11:41:25 +0000

    Append log

--- ansible/roles/master/files/usr/sbin/slurm_resume.sh
+++ ansible/roles/master/files/usr/sbin/slurm_resume.sh
@@ -4,6 +4,6 @@ echo "`date` Resume invoked $0 $*" >>/var/log/power_save.log
 hosts=`scontrol show hostnames $1`
 for host in $hosts
 do
-   timeout 10m /usr/sbin/start_instance.py $host &
+   timeout 10m /usr/sbin/start_instance.py $host 2>&1 >> /var/log/power_save.log &
 done
 wait
--- ansible/roles/master/files/usr/sbin/slurm_suspend.sh
+++ ansible/roles/master/files/usr/sbin/slurm_suspend.sh
@@ -4,6 +4,6 @@ echo "`date` Suspend invoked $0 $*" >>/var/log/power_save.log
 hosts=`scontrol show hostnames $1`
 for host in $hosts
 do
-   timeout 10m /usr/sbin/stop_instance.py $host &
+   timeout 10m /usr/sbin/stop_instance.py $host 2>&1 >> /var/log/power_save.log &
 done
 wait
